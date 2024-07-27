2885e0d804edb5005e9bead73408c70d6bbc920c
Alexander <alekogureshidze@gmail.com>
2024-07-27 16:59:43 +0400

    Add tests

---
+++ my_project/my_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+namespace
+{
+    int GetMeaningOfLife() {return 42;}
+} // namespace
+
+TEST(TestTopic, TrivialEquality)
+{
+    EXPECT_EQ(GetMeaningOfLife(), 42);
+}
+
+TEST(TestTopic, MoreEqualityTests)
+{
+    ASSERT_EQ(GetMeaningOfLife(), 0) << "Oh no, a mistake!";
+}
